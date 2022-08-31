3d423c7096bfd472f8de31b454983a2a9683ed4b
rparak <59449320+rparak@users.noreply.github.com>
2022-08-31 17:15:47 +0100

    cleaning parts of python

--- src/Python/Converter/Converter.py
+++ src/Python/Converter/Converter.py
@@ -27,7 +27,6 @@ File Name: Converter.py
 
 # System (Default Lib.)
 import sys
-
 # Numpy (Array computing Lib.) [pip3 install numpy]
 import numpy as np
 
@@ -35,8 +34,9 @@ import numpy as np
 Description:
     A byte is a unit of storage in a computer which contains 8-bits and can store 256 different values: 0 to 255. 
 """
-NUM_OF_VALUES_IN_BYTE = 256
-NUM_OF_BIT_IN_BYTE    = 8
+CONST_NUM_OF_VALUES_IN_BYTE = 256
+CONST_NUM_OF_BIT_IN_BYTE    = 8
+CONST_DATA_TYPE_LOWER_LIMIT = 0
 
 """
 Notes:
@@ -44,131 +44,145 @@ Notes:
     UINT : Unsigned INT          [0, 65535]
     UDINT: Unsigned (double) INT [0, 4294967295]
 """
-BOOL_UPPER_LIMIT      = 1
-USINT_UPPER_LIMIT     = 255
-UINT_UPPER_LIMIT      = 65535
-UDINT_UPPER_LIMIT     = 4294967295
-DATA_TYPE_LOWER_LIMIT = 0
+CONST_BOOL_UPPER_LIMIT      = 1
+CONST_USINT_UPPER_LIMIT     = np.iinfo(np.uint8).max 
+CONST_UINT_UPPER_LIMIT      = np.iinfo(np.uint16).max 
+CONST_UDINT_UPPER_LIMIT     = np.iinfo(np.uint32).max 
 
 def Convert_NUM_To_Multiple_BYTES(in_num, type_id):
     """
     Description:
-        Function for converting uint/udint (16-bit -> 2-byte / 32-bit -> 4-byte) to multiple bytes.
+        Conversion of input value (UINT/UDINT) into a vector of values (BYTES).
+
+        Note:
+            UINT: 16-bit -> 2-byte
+            UDINT: 32-bit -> 4-byte
         
     Args:
         (1) in_num [UINT / UDINT]: Input number (2 -> possible values range from 0 to 65535 / 4 BYTEs -> possible values range from 0 to 4294967295).
-        (2) type_id [USINT {Byte}]: Type of calculation. Identification number: 2 -> 2 BYTEs OUT, 4 -> BYTEs OUT
+        (2) type_id [USINT {Byte}]: Type of calculation. 
+                                    Identification number: 2 -> 2 BYTEs OUT, 4 -> BYTEs OUT
         
     Returns:
-        (1) out [USINT {Byte} Array (2, 4)]: Output multiple numbers. Possible values range from 0 to 255 in each array index.
+        (1) parameter [USINT {Byte} Array (2, 4)]: Vector of values (BYTES). Possible values range from 0 to 255 in each array index.
     """
  
     try:
         assert type_id == 2 or type_id == 4
-        assert ((type_id == 2 and (DATA_TYPE_LOWER_LIMIT <= in_num <= UINT_UPPER_LIMIT)) or (type_id == 4 and (DATA_TYPE_LOWER_LIMIT <= in_num <= UDINT_UPPER_LIMIT)))
+        assert ((type_id == 2 and (CONST_DATA_TYPE_LOWER_LIMIT <= in_num <= CONST_UINT_UPPER_LIMIT)) or \
+                (type_id == 4 and (CONST_DATA_TYPE_LOWER_LIMIT <= in_num <= CONST_UDINT_UPPER_LIMIT)))
 
-        # Create aux. variable
-        aux_var = int(in_num/NUM_OF_VALUES_IN_BYTE)
+        # Create aux. variable.
+        aux_var = int(in_num/CONST_NUM_OF_VALUES_IN_BYTE)
 
-        # Initialize the first index of the output array
-        out    = np.zeros(type_id)
-        out[0] = int(in_num) % NUM_OF_VALUES_IN_BYTE
+        out_num_arr = np.zeros(type_id, dtype = np.uint8)
+        # Calculate the first index of the output array.
+        out_num_arr[0] = int(in_num) % CONST_NUM_OF_VALUES_IN_BYTE
         
         for i in range(1, type_id):
-            out[i]   = int(aux_var) % NUM_OF_VALUES_IN_BYTE
-            aux_var /= NUM_OF_VALUES_IN_BYTE
+            out_num_arr[i] = int(aux_var) % CONST_NUM_OF_VALUES_IN_BYTE
+            aux_var /= CONST_NUM_OF_VALUES_IN_BYTE
         
-        return out
+        return out_num_arr
 
     except AssertionError as error:
         print('[ERROR] Invalid input. Please try again ...')
+        print(f'[ERROR] Information: {error}')
 
 def Convert_Multiple_BYTES_To_NUM(in_byte_arr):
     """
     Description:
-        Function for converting multiple bytes to an uint/udint (16-bit -> 2-byte / 32-bit -> 4-byte).
+        Conversion of a vector of values (BYTES) to a value (UINT/UDINT).
+
+        Note:
+            UINT: 16-bit -> 2-byte
+            UDINT: 32-bit -> 4-byte
         
     Args:
         (1) in_byte_arr [USINT {Byte} Array (2, 4)]: Input multiple numbers. Possible values range from 0 to 255 in each array index.
         
     Returns:
-        (1) out [UINT / UDINT]: Output number (2 -> possible values range from 0 to 65535 / 4 BYTEs -> possible values range from 0 to 4294967295).
+        (1) parameter [UINT / UDINT]: Output number (2 -> possible values range from 0 to 65535 / 4 BYTEs -> possible values range from 0 to 4294967295).
     """
         
     try: 
         assert in_byte_arr.size == 2 or in_byte_arr.size == 4
 
-        out = 0
+        out_num = 0
         for i, in_byte_arr_i in enumerate(in_byte_arr):
-            if DATA_TYPE_LOWER_LIMIT > in_byte_arr_i > USINT_UPPER_LIMIT:
+            if CONST_DATA_TYPE_LOWER_LIMIT > in_byte_arr_i > CONST_USINT_UPPER_LIMIT:
                 assert False
 
-            out += in_byte_arr_i*(NUM_OF_VALUES_IN_BYTE**i)
+            out_num += in_byte_arr_i * (CONST_NUM_OF_VALUES_IN_BYTE**i)
             
-        return out
+        return out_num
 
     except AssertionError as error:
         print('[ERROR] Invalid input. Please try again ...')
+        print(f'[ERROR] Information: {error}')
 
 def Convert_BYTE_To_Multiple_BITS(in_byte):
     """
     Description:
-        Conversion of input values (BYTE) into a vector of logical values (BIT).
+        Conversion of input value (BYTE) into a vector of logical values (BITS).
         
     Args:
         (1) in_byte [USINT {Byte}]: Input number (BYTE -> possible values range from 0 to 255). 
         
     Returns:
-        (1) out [BOOL {Bit} Array (0 .. 7)]: Output multiple bits (1 BYTE).
+        (1) parameter [BOOL {Bit} Array (0 .. 7)]: Output multiple bits (1 BYTE).
     """
         
     try: 
-        assert DATA_TYPE_LOWER_LIMIT <= in_byte <= USINT_UPPER_LIMIT
+        assert CONST_DATA_TYPE_LOWER_LIMIT <= in_byte <= CONST_USINT_UPPER_LIMIT
 
         # Create aux. variable
         aux_byte = in_byte
         
-        # Convert decimal number (BYTE) to multiple Booleans (BITs)
+        # Convert decimal number (BYTE) to multiple Booleans (BITs).
         # Note: 1 BYTE [0 - 255] = 8 BITs [0 - 1]
-        out = np.zeros(NUM_OF_BIT_IN_BYTE, dtype=np.bool_)
+        out_bit_arr = np.zeros(CONST_NUM_OF_BIT_IN_BYTE, dtype=np.bool_)
         
-        for i in range(NUM_OF_BIT_IN_BYTE):
-            out[i]   = aux_byte % 2
+        for i in range(CONST_NUM_OF_BIT_IN_BYTE):
+            out_bit_arr[i] = aux_byte % 2
             aux_byte = int(aux_byte / 2)
 
-        return out
+        return out_bit_arr
 
     except AssertionError as error:
         print('[ERROR] Invalid input. Please try again ...')
+        print(f'[ERROR] Information: {error}')
 
-def Convert_Multiple_BITS_To_BYTE(in_bit):
+def Convert_Multiple_BITS_To_BYTE(in_bit_arr):
     """
     Description:
-        Function for converting multiple bits to byte.
+        Conversion of a vector of logical values (BITS) to a value (BYTE).
         
     Args:
-        (1) in_bit [BOOL {Bit} Array (0 .. 7)]: Input multiple bits (1 BYTE).
+        (1) in_bit_arr [BOOL {Bit} Array (0 .. 7)]: Input multiple bits (1 BYTE).
         
     Returns:
-        (1) out [USINT {Byte}]: Output number (BYTE -> possible values range from 0 to 255). 
+        (1) parameter [USINT {Byte}]: Output number (BYTE -> possible values range from 0 to 255). 
     """
+
     try: 
-        assert in_bit.size == NUM_OF_BIT_IN_BYTE
+        assert in_bit_arr.size == CONST_NUM_OF_BIT_IN_BYTE
 
         # Convert multiple Booleans (BITs) to decimal number (BYTE)
         # Note: 8 BITs [0 - 1] = 1 BYTE [0 - 255]
-        out = 0
+        out_byte = 0
 
-        for i, in_bit_i in enumerate(in_bit):
-            if DATA_TYPE_LOWER_LIMIT < in_bit_i < BOOL_UPPER_LIMIT:
+        for i, in_bit_arr_i in enumerate(in_bit_arr):
+            if CONST_DATA_TYPE_LOWER_LIMIT < in_bit_arr_i < CONST_BOOL_UPPER_LIMIT:
                 assert False
 
-            out += 2**i if in_bit_i == BOOL_UPPER_LIMIT else DATA_TYPE_LOWER_LIMIT
+            out_byte += 2**i if in_bit_arr_i == CONST_BOOL_UPPER_LIMIT else CONST_DATA_TYPE_LOWER_LIMIT
 
-        return out
+        return out_byte
 
     except AssertionError as error:
         print('[ERROR] Invalid input. Please try again ...')
+        print(f'[ERROR] Information: {error}')
 
 def main():
     """
@@ -177,12 +191,13 @@ def main():
     """
 
     # 1\ Test No. 1: UINT <-> BYTE[]
-    # Converting uint (16-bit -> 2-byte) to multiple bytes.
+    #   Converting uint (16-bit -> 2-byte) to multiple bytes.
     input_var = 12345
     input_tId = 2
     res_tId_2 = Convert_NUM_To_Multiple_BYTES(input_var, input_tId)
 
-    # Check result: Converting multiple bytes to an uint (16-bit -> 2-byte).
+    # Check result: 
+    #   Converting multiple bytes to an uint (16-bit -> 2-byte).
     check_res_tId_2 = Convert_Multiple_BYTES_To_NUM(res_tId_2)
 
     # Compare results
@@ -193,12 +208,13 @@ def main():
         print('[INFO] Something is wrong.')
 
     # 2\ Test No. 2: UDINT <-> BYTE[]
-    # Converting udint (32-bit -> 4-byte) to multiple bytes.
+    #   Converting udint (32-bit -> 4-byte) to multiple bytes.
     input_var = 12345678
     input_tId = 4
     res_tId_4 = Convert_NUM_To_Multiple_BYTES(input_var, input_tId)
 
-    # Check result: Converting multiple bytes to an udint (32-bit -> 4-byte).
+    # Check result: 
+    #   Converting multiple bytes to an udint (32-bit -> 4-byte).
     check_res_tId_4 = Convert_Multiple_BYTES_To_NUM(res_tId_4)
 
     # Compare results
@@ -209,11 +225,12 @@ def main():
         print('[INFO] Something is wrong.')
 
     # 3\ Test No. 3:  BYTE <-> BIT[]
-    # Converting byte to multiple bits.
+    #   Converting byte to multiple bits.
     input_byte  = 123
     res_bit_arr = Convert_BYTE_To_Multiple_BITS(input_byte)
 
-    # Check result: Converting multiple bits to byte.
+    # Check result: 
+    #   Converting multiple bits to byte.
     check_res = Convert_Multiple_BITS_To_BYTE(res_bit_arr)
 
     # Compare results
--- src/Python/Helpers/var_map_calculator.py
+++ src/Python/Helpers/var_map_calculator.py
@@ -27,68 +27,57 @@ File Name: var_map_calculator.py
 
 # System (Default Lib.)
 import sys
-
 # Numpy (Array computing Lib.) [pip3 install numpy]
 import numpy as np
 
-def VM_Write(limit, rob_type):
+# Default Bit Size (1 Byte = 8 bits)
+CONST_NUM_OF_BIT_IN_BYTE = 8
+# Initialization variables for output information about the results
+CONST_DATA_TYPE = [['USINT', 'UINT', 'None', 'UDINT'], 
+                   ['USINT', 'INT', 'None', 'REAL']]
+CONST_RANGE     = [['[0, +255]', '[0, +65535]', 'None', '[0, +4294967295]'],
+                   ['[0, +255]', '[-32768, +32767]', 'None', '[-3.4E+38, +3.4E+38]']]
+CONST_SIZE      = ['8', '16' , 'None', '32']
+
+def VM_Write(limit):
     """
     Description:
         Simple function to calculate mapping variables (addresses) for Ethernet protocols 
         such as PROFINET, EtherNet/IP.
 
-        PLC -> Robot, Controller, etc. 
+        Note:
+            PLC -> Robot, Controller, etc. 
             
     Args:
         (1) limit [INT Array]: Limit (upper / lower) mapping variables in bytes.
-        (2) rob_type [INT]: Type of the robot (controller). 
 
     Examples:
         # Lower Limit: Byte 0
         # Upper Limit: Byte 3
-         # Type of the robot (controller): 0 (ABB), 1 (Universal Robots) 
+        # Type of the robot (controller): 0 (ABB), 1 (Universal Robots) 
         VM_Write([0, 3], 0)
     """
 
     try:
-        # Tests of input variables
-        assert len(limit) == 2
+        assert limit.size == 2
         assert limit[0] == limit[1] or (limit[0] < limit[1] and (limit[0] and limit[1]) >= 0)
 
         if limit[0] != limit[1]:
             assert ((limit[1] - limit[0]) + 1) % 2 == 0
 
-        # Information about input variables
-        print('[INFO] Limit (upper / lower) in bytes: [%d, %d]' % (limit[0], limit[1]))
-        print('[INFO] Type of the robot (controller): %d' % rob_type)
-
-        # Initialization variables for output information about the results
-        DATA_TYPE = [['USINT', 'UINT', 'None', 'UDINT'], ['USINT', 'INT', 'None', 'REAL']]
-        RANGE     = [['[0, +255]', '[0, +65535]', 'None', '[0, +4294967295]'], ['[0, +255]', '[-32768, +32767]', 'None', '[-3.4E+38, +3.4E+38]']]
-        SIZE      = [['8', '16' , 'None', '32'], ['8', '16' , 'None', '32']]
-
-        # Default Bit Size (1 Byte = 8 bits)
-        default_bitSize = 8
-
-        # Number of variables used to transfer information between the Robot and the PLC
+        # Number of variables used to transfer information between the Robot and the PLC.
         num_ofVar = ((limit[1] - limit[0]) + 1)
 
-        # The resulting calculation of the equation
-        result = [limit[0] * default_bitSize, (limit[1] + 1) * (default_bitSize) - 1]
+        # The resulting calculation of the equation.
+        result_adr = np.array([limit[0] * CONST_NUM_OF_BIT_IN_BYTE, (limit[1] + 1) * (CONST_NUM_OF_BIT_IN_BYTE) - 1])
 
-        # Display Results (Output)
-        print('[INFO] Number of Variables: %d' % num_ofVar)
-        print('[INFO] Data Type: %s' % DATA_TYPE[rob_type][num_ofVar - 1])
-        print('[INFO] Range: %s' % RANGE[rob_type][num_ofVar - 1])
-        print('[INFO] Size (bits): %s' % SIZE[rob_type][num_ofVar - 1])
-        print('[INFO] Variable Mapping (Bits):')
-        print('[INFO] Address (Lower): %d' % result[0])
-        print('[INFO] Address (Upper): %d' % result[1])
+        return (num_ofVar, result_adr)
 
     except AssertionError as error:
-        print('[ERROR] Invalid operation (problem with input variables).')  
+        print('[ERROR] Invalid input. Please try again ...')
+        print(f'[ERROR] Information: {error}')
 
-def VM_Read(limit, rob_type):
+def VM_Read(limit):
     """
     Description:
         Simple function to calculate mapping variables (addresses) for Ethernet protocols 
@@ -98,52 +87,31 @@ def VM_Read(limit, rob_type):
 
     Args:
         (1) limit [INT Array]: Limit (upper / lower) mapping variables in bits.
-        (2) rob_type [INT]: Type of the robot (controller). 
 
     Examples:
         # Lower Limit: Bit 0
         # Upper Limit: Bit 31
-        # Type of the robot (controller): 0 (ABB), 1 (Universal Robots) 
-         VM_Read([0, 31], 0)
+        VM_Read([0, 31], 0)
     """
 
     try:
-        # Tests of input variables
-        assert len(limit) == 2
+        assert limit.size == 2
         assert limit[0] == limit[1] or (limit[0] < limit[1] and (limit[0] and limit[1]) >= 0)
 
         if limit[0] != limit[1]:
             assert ((limit[1] - limit[0]) + 1) % 2 == 0
 
-        # Information about input variables
-        print('[INFO] Limit (upper / lower) in bits: [%d, %d]' % (limit[0], limit[1]))
-        print('[INFO] Type of the robot (controller): %d' % rob_type)
+        # Number of variables used to transfer information between the Robot and the PLC.
+        num_ofVar = int(((limit[1] - limit[0]) + 1) / CONST_NUM_OF_BIT_IN_BYTE)
 
-        # Initialization variables for output information about the results
-        DATA_TYPE = [['USINT', 'UINT', 'None', 'UDINT'], ['USINT', 'INT', 'None', 'REAL']]
-        RANGE     = [['[0, +255]', '[0, +65535]', 'None', '[0, +4294967295]'], ['[0, +255]', '[-32768, +32767]', 'None', '[-3.4E+38, +3.4E+38]']]
-        SIZE      = [['8', '16' , 'None', '32'], ['8', '16' , 'None', '32']]
+        # The resulting calculation of the equation.
+        result_adr = np.array([limit[0] / CONST_NUM_OF_BIT_IN_BYTE, (limit[1] + 1) / (CONST_NUM_OF_BIT_IN_BYTE) - 1])
 
-        # Default Bit Size (1 Byte = 8 bits)
-        default_bitSize = 8
-
-        # Number of variables used to transfer information between the Robot and the PLC
-        num_ofVar = int(((limit[1] - limit[0]) + 1) / default_bitSize)
-
-        # The resulting calculation of the equation
-        result = [limit[0] / default_bitSize, (limit[1] + 1) / (default_bitSize) - 1]
-
-        # Display Results (Output)
-        print('[INFO] Number of Variables: %d' % num_ofVar)
-        print('[INFO] Data Type: %s' % DATA_TYPE[rob_type][num_ofVar - 1])
-        print('[INFO] Range: %s' % RANGE[rob_type][num_ofVar - 1])
-        print('[INFO] Size (bits): %s' % SIZE[rob_type][num_ofVar - 1])
-        print('[INFO] Variable Mapping (Bytes):')
-        print('[INFO] Address (Lower): %d' % result[0])
-        print('[INFO] Address (Upper): %d' % result[1])
+        return (num_ofVar, result_adr)
 
     except AssertionError as error:
-        print('[ERROR] Invalid operation (problem with input variables).')
+        print('[ERROR] Invalid input. Please try again ...')
+        print(f'[ERROR] Information: {error}')
 
 def main():
     """
@@ -152,16 +120,51 @@ def main():
         such as PROFINET, EtherNet/IP. 
     """
 
+    # Declaration of input variables.
+    #   Limit (upper / lower) mapping variables in bytes.
+    in_limit    = np.array([0, 3])
+    #   Type of the robot (controller): 
+    #       0 (ABB), 1 (Universal Robots) 
+    in_rob_type = 0
+
+    # Information about input variables
+    print('[INFO] Limit (upper / lower) in bits: [%d, %d]' % (in_limit[0], in_limit[1]))
+    print('[INFO] Type of the robot (controller): %d' % in_rob_type)
+
     # PLC -> Robot (Variable Mapping)
-    #print('[INFO] Calculation Type (Variable Mapping - PROFINET): PLC -> Robot:')
-    
-    VM_Write([7, 7], 0)
-    print('\n')
+    print('[INFO] Calculation Type (Variable Mapping - PROFINET): PLC -> Robot:')
+    # 1\ Test.
+    vm_w_res = VM_Write(in_limit)
+
+    # Display Additional informations (Output function 1\).
+    print('[INFO]   Number of Variables: %d' % vm_w_res[0])
+    print('[INFO]   Data Type: %s' % CONST_DATA_TYPE[in_rob_type][vm_w_res[0] - 1])
+    print('[INFO]   Range: %s' % CONST_RANGE[in_rob_type][vm_w_res[0] - 1])
+    print('[INFO]   Size (bits): %s' % CONST_SIZE[vm_w_res[0] - 1])
+    print('[INFO]   Variable Mapping (Bytes):')
+    print('[INFO]   Address (Lower): %d' % vm_w_res[1][0])
+    print('[INFO]   Address (Upper): %d' % vm_w_res[1][1])
 
     # Robot -> PLC (Variable Mapping)
-    #print('[INFO] Calculation Type (Variable Mapping - PROFINET): Robot -> PLC:')
-    #VM_Read([656,663], 0)
-    #print('\n')
+    print('[INFO] Calculation Type (Variable Mapping - PROFINET): Robot -> PLC:')
+    # 2\ Test.
+    vm_r_res = VM_Read(vm_w_res[1])
 
+    # Display Additional informations (Output function 1\).
+    print('[INFO]   Number of Variables: %d' % vm_r_res[0])
+    print('[INFO]   Data Type: %s' % CONST_DATA_TYPE[in_rob_type][vm_r_res[0] - 1])
+    print('[INFO]   Range: %s' % CONST_RANGE[in_rob_type][vm_r_res[0] - 1])
+    print('[INFO]   Size (bits): %s' % CONST_SIZE[vm_r_res[0] - 1])
+    print('[INFO]   Variable Mapping (Bytes):')
+    print('[INFO]   Address (Lower): %d' % vm_r_res[1][0])
+    print('[INFO]   Address (Upper): %d' % vm_r_res[1][1])
+
+    # Compare results
+    print('[INFO] Test No. 1 / 2:')
+    if vm_r_res[1].all() == in_limit.all():
+        print('[INFO] The calculation was completed successfully.')
+    else:
+        print('[INFO] Something is wrong.')
+        
 if __name__ == '__main__':
     sys.exit(main())
